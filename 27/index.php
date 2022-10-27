db5c82880c0d29d853e58edf2882946ef8661248
darshandeep <69204905+darshandeep@users.noreply.github.com>
2022-10-27 20:00:38 +0530

    Add files via upload

---
+++ 27/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <!-- <div class="navbar">
+        <div class="navbar-left">
+            <div class="navbar-left-img"><img class="navbar-left-img-child" src="" alt="" srcset=""></div>
+            <div class="navbar-left-btn"><button class="navbar-left-btn-child">+Blog</button></div>
+        </div>
+    </div> -->
+
+</head>
+
+<body >
+<div class="body-flex">
+<!-- sidenav-->
+<?php
+        include('nav.php');
+
+    ?>
+    
+    <!-- <div class="side-nav">
+       <a href="about.php"> <div  class="navbar-home"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="home" srcset=""></div></a>
+       <a href="contact.php"> <div class="navbar-about"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="about"></div></a>
+        <div class="navbar-blog"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="blog"></div>
+        <div class="navbar-contact"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="contact"></div>
+
+    </div> -->
+    <!-- sidenav-->
+
+
+    <!--mid content-->
+    <div class="content">
+        <div class="heading-topic-time">
+            <div class="heading-topic">Crafting</div>
+            <div class="heading-time">. February 12,2022</div>
+        </div>
+
+
+        <div class="text-header-para">
+            <div class="text-header-heading">Whatever is begun in anger ends in shame</div>
+            <div class="text-header-para-child">Now when I had mastered the language of this water and had come to know every
+                trifling feature that bordered the great river as familiarly as I knew the letters of the alphabet, I
+                had made a valuable acquisition. I still keep in mind a certain wonderful sunset which I witnessed when
+                and steamboating <div class="right-arrow-img"><img style="margin-top: 4.5rem; margin-left:-4rem ; " class="right-arrow-img-child" src="" alt="--->">
+                </div>
+            </div>
+        </div>
+
+        <!-- X4 -->
+        <div class="full-text-list">
+            <!-- flex down -->
+            <div class="text-list-box-1">
+                <div class="text-list-box-1-heading">I like to reinvent myself</div>
+                <div class="text-list-box-1-para">
+                    <!-- flex side -->
+                    <div class="text-list-box-1-para-topic">SCIENCE</div>
+
+                    <div class="text-list-box-1-para-date">.&nbsp;&nbsp;&nbsp;&nbsp;FEBRUARY 12, 2016</div>
+                </div>
+            </div>
+
+            <div class="text-list-box-1">
+                <div class="text-list-box-1-heading">I like to reinvent myself</div>
+                <div class="text-list-box-1-para">
+                    <!-- flex side -->
+                    <div class="text-list-box-1-para-topic">SCIENCE</div>
+
+                    <div class="text-list-box-1-para-date">.&nbsp;&nbsp;&nbsp;&nbsp;FEBRUARY 12, 2016</div>
+                </div>
+            </div>
+
+            <div class="text-list-box-1">
+                <div class="text-list-box-1-heading">I like to reinvent myself</div>
+                <div class="text-list-box-1-para">
+                    <!-- flex side -->
+                    <div class="text-list-box-1-para-topic">SCIENCE</div>
+
+                    <div class="text-list-box-1-para-date">.&nbsp;&nbsp;&nbsp;&nbsp;FEBRUARY 12, 2016</div>
+                </div>
+            </div>
+        
+
+
+
+
+        
+            <div class="text-list-box-1">
+                <div class="text-list-box-1-heading">I like to reinvent myself</div>
+                <div class="text-list-box-1-para">
+                    <!-- flex side -->
+                    <div class="text-list-box-1-para-topic">SCIENCE</div>
+
+                    <div class="text-list-box-1-para-date">.&nbsp;&nbsp;&nbsp;&nbsp;FEBRUARY 12, 2016</div>
+                </div>
+            </div>
+            <div class="text-list-box-1">
+                <div class="text-list-box-1-heading">I like to reinvent myself</div>
+                <div class="text-list-box-1-para">
+                    <!-- flex side -->
+                    <div class="text-list-box-1-para-topic">SCIENCE</div>
+
+                    <div class="text-list-box-1-para-date">.&nbsp;&nbsp;&nbsp;&nbsp;FEBRUARY 12, 2016</div>
+                </div>
+            </div>
+        
+        
+           
+        </div>
+
+        
+        
+        
+        
+        
+        
+        
+        
+       
+
+
+
+        
+
+        <!-- X4 -->
+    </div>
+    <!--mid content-->
+
+    <div class="bg-img"><img class="bg-img-child"
+            src="pic.png"
+            alt="" srcset="">
+
+    </div>
+
+
+</body>
+
+</html>
