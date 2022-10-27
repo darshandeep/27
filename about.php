7e2db745057f85bf5d9855b7d8b0c8171c2df923
darshandeep <69204905+darshandeep@users.noreply.github.com>
2022-10-27 20:13:38 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <div class="body-flex" >
+        <!-- sidenav-->
+        <div class="side-nav">
+           <a href="index.php"> <div  class="navbar-home"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="home" srcset=""></div></a>
+           <a href="contact.php"> <div class="navbar-about"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="about"></div></a>
+            <div class="navbar-blog"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="blog"></div>
+            <div class="navbar-contact"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="contact"></div>
+    
+        </div></div>
+
+    
+    <div class="big-img"><img style="margin-left: 6.2rem; margin-top: -185%;" class="big-img-child2"
+            src="https://preview.colorlib.com/theme/suppablog/demo-images/01.jpg" alt="" srcset=""></div>
+
+
+</head>
+
+<body style="color: white;">
+
+    <div class="about-text" style="margin-left: 20rem;margin-bottom: 0rem; margin-top: -70%;">
+        <div class="about-header-text">About Suppablog</div>
+        <div class="about-para">
+            <div class="about-para-1">
+                We choose to go to the moon in this decade and do the other things, not because they are easy.
+                <br><br>
+                Because they are hard, because that goal will serve to organize and measure the best of our energies and
+                skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone
+                and one.
+            </div>
+            <div class="about-para-2">Never in all their history have men been able truly to conceive of the world as
+                one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the
+                directions eventually meet, in which there is no center because every point, or none, is center an equal
+                earth which all men occupy as equals. The airmans earth, if free men make it, will be truly round.
+            </div>
+        </div>
+    </div>
+
+    <div class="about-review-box" style="margin-left: 17rem; margin-bottom: 0rem;">
+        <div class="about-review-img"><img class="about-review-img-child"
+                src="https://preview.colorlib.com/theme/suppablog/demo-images/profile_01.jpg" alt=""></div>
+        <div class="h">
+            <div class="g">
+                <div class="about-review-heading-1">JENN MARSHALL</div>
+                <br>
+                <div class="about-review-heading-2">AUTHOR / BLOGGER</div>
+            </div>
+
+            <div class="about-review-para">Lorem ipsum dolor sit amet, consectetur perset adipisicing elit, sed do
+                eiusmod
+                tempor.
+                <br>
+                <br>
+
+                Ut enim aden minim veniam, quis nostrud seti exercitation ullamco laboris nisi ut aliquip exeea commodo
+                consequat. Duis aute irure dolor.
+                <br>
+                <br>
+
+                Suspendisse potenti. Maecenas faucibus massa euismod, malesuada diam finibus, semper ligula. Donec nibh
+                arcu, scelerisque id nisl sit amet, placerat mollis neque. Nullam suscipit auctor laoreet.</div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ text-blog.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+<div class="client-header-box-1">
+        
+        <div class="client-box-flex">
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                        
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                        
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                        
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+
+
+            
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1"> Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="client-box-flex">
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1" style="color:white ; background-color:black "> Lorem ipsum dolor sit amet
+                    consectetur.</div>
+            </div>
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1" style="color:white ; background-color:black "> Lorem ipsum dolor sit amet
+                    consectetur.</div>
+            </div>
+
+            
+
+            
+
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1" style="color:white ; background-color:black "> Lorem ipsum dolor sit amet
+                    consectetur.</div>
+            </div>
+
+
+            <div class="client-box">
+                <div class="box-img-1-container"> <img class="box-img-1"
+                        src="https://uploads-ssl.webflow.com/60351be25d61ccd82f3b69de/6261450ccf63164aa06dc35a_inflow%20logo.svg"
+                        alt="" srcset=""></div>
+
+                <div class="box-text-1" style="color:white ; background-color:black "> Lorem ipsum dolor sit amet
+                    consectetur.</div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+    </div>
+</body>
+</html>
