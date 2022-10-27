157da678f64c58eb0f1608e65e26c529feba03dc
darshandeep <69204905+darshandeep@users.noreply.github.com>
2022-10-27 20:11:46 +0530

    Delete 27 directory

--- 27/nav.php
+++
@@ -1,7 +0,0 @@
-<div class="side-nav" >
-       <a href="about.php"> <div  class="navbar-home"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="home" srcset=""></div></a>
-       <a href="contact.php"> <div class="navbar-about"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="about"></div></a>
-       <a href="text-blog.php"> <div class="navbar-blog"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="blog"></div></a>
-        <div class="navbar-contact"><img style="width:60% ; background-color: rgb(255, 255, 255); " src="square.png" alt="contact"></div>
-
-    </div>
